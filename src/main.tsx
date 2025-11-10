a9a0fe079fa7deb1e61d3931b58b54aa37d3eedf
Alireza <alireza4o4shabani@gmail.com>
2025-11-11 00:54:17 +0330

    Third Commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { ApolloProvider } from "@apollo/client";
 import { Provider } from "react-redux";
 
 import Store from "./Redux/Store.ts";
-import Client from "./GraphQL/Client.ts";
 import App from "./App.tsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <ApolloProvider client={Client}>
-    <Provider store={Store}>
-      <App />
-    </Provider>
-  </ApolloProvider>
+  <Provider store={Store}>
+    <App />
+  </Provider>
 );
